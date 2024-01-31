c7c45cc86cba6ae3e07235f404f11b85c0f75d1f
Fengdi Huang <96247250+JesseLovesGrace@users.noreply.github.com>
2024-01-31 22:02:41 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+API_KEY = "YOUR API KEY"
+SECRET_KEY = "YOUR SECRET KEY"
+
+ALPACA_CONFIG = {
+# Put your own Alpaca key here:
+"API_KEY": "YOUR API KEY",
+# Put your own Alpaca secret here:
+"API_SECRET": "YOUR API SECRET KEY",
+# If you want to go live, you must change this to real live trading and link your account
+"ENDPOINT": "https://paper-api.alpaca.markets",
+}
